c51cd2792c6ae9143a04a42297339c1080db3782
Jenny Bulford <jennycbx@hotmail.com>
2021-03-23 18:53:49 +0000

    Can count books in library

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Books> books;
+
+    public Library() {
+        this.books = new ArrayList<Books>();
+    }
+
+    public int numberOfBooks() {
+        return this.books.size();
+    }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class LibraryTest {
+
+    Library library;
+    Books book;
+    Books book1;
+    Books book2;
+
+    @Before
+    public void setUp() {
+        library = new Library();
+        book = new Books("Harry Potter", "J.K.Rowling", "Fantasy");
+        book1 = new Books("Cooking", "Gordon Ramsay", "Educational");
+        book2 = new Books("Me Before You", "JoJo Moyes", "Romance");
+    }
+
+    @Test
+    public void booksInLibrary() {
+        assertEquals(0, library.numberOfBooks());
+    }
 }
